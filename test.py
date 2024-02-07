898f8d520aa068df4997b4e05240767759220e89
SesyuninaM <Marishka-MSV@yandex.ru>
2024-02-07 17:58:03 +0300

    third change input

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-name = input ("What's your name?")
-print ("Hello!", name, "!")
-print ("We learn Git ")
-print ("We learn GitHub")
+name = input("What's your name? ")
+print("Hello, ", name, "!")
+print("We learn Git")
+Print("We learn GitHub")
